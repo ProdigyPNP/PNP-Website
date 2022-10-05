0f0468bcb3c14e7c9e5b8a4159600dad480559e1
gemsvido <gemsvido@gmail.com>
2022-10-05 08:50:46 -0700

    Fix edit this page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,14 +37,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/ProdigyPNP/ProdigyPNP.github.io',
+            'https://github.com/ProdigyPNP/ProdigyPNP.github.io/tree/master',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/ProdigyPNP/ProdigyPNP.github.io',
+            'https://github.com/ProdigyPNP/ProdigyPNP.github.io/tree/master',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
